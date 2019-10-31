ece290bc5e3850140913e5cef35b060b8be3a2dd
edwiinn <ykedwinh@gmail.com>
2019-11-01 02:58:04 +0700

    test automated pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelu
+                    Laravelo
                 </div>
 
                 <div class="links">
