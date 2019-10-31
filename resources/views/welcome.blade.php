af58256ca0b6ac7b52fe928b3dfd7aa6f9a6551a
edwiinn <ykedwinh@gmail.com>
2019-11-01 02:45:48 +0700

    change welcome name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravela
                 </div>
 
                 <div class="links">
