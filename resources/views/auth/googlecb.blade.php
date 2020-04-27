d51993a3d5e3994f70e488e997c663a198c29cec
edwinh <ykedwinh@gmail.com>
2020-04-27 23:55:35 +0700

    Update scheme

--- resources/views/auth/googlecb.blade.php
+++ resources/views/auth/googlecb.blade.php
@@ -1,1 +1,1 @@
-<a href="intent:#Intent;schemeName://com.google.codelabs.appauth;action=com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE;package=com.edwiinn.project;end">Link back to my app</a>
+<a href="intent:#Intent;scheme=com.google.codelabs.appauth;action=com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE;package=com.edwiinn.project;end">Link back to my app</a>
